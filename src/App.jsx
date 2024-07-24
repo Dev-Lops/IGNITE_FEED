a95ea08db6d0c61b6ef789e1673afb5ff3394862
Anderson Lopes <developes.anderson@gmail.com>
2024-07-24 00:02:46 -0400

    🚀 Projeto Finalizado

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { Header } from './components/Header';
+
+import styles from './App.module.css';
+import { Post } from './components/Post';
+
+import { Sidebar } from './components/Sidebar';
+import './global.css';
+
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/lopesa95.png',
+      name: 'Anderson Lopes',
+      role: 'Developer Front End'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋 ' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2024-02-11 08:13'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'Developer Front End'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋 ' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2024-01-11 08:13'),
+  },
+];
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main >
+          {posts.map(post => {
+            return (< Post
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />)
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
